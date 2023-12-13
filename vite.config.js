6f2e7b24695e872602cf391e9bcbae3c33e14faf
Sandeep <sandeep@StudyMonks-MacBook-Pro.local>
2023-12-13 23:46:06 +0530

    set chunk size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit:500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
