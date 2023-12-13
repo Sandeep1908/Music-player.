35804aff208b28f7f3535dbab1c0099ac0ed4602
Sandeep <sandeep@StudyMonks-MacBook-Pro.local>
2023-12-13 23:32:54 +0530

    add react-script

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Route, Routes, useNavigate, HashRouter } from "react-router-dom";
 
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
-import { LoginMobile } from './pages/Login';
-import { onAuthChanged } from './firebase';
-import {auth} from './firebase'
-import { useEffect } from 'react';
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from "./components";
+import {
+  ArtistDetails,
+  TopArtists,
+  AroundYou,
+  Discover,
+  Search,
+  SongDetails,
+  TopCharts,
+} from "./pages";
+import { LoginMobile } from "./pages/Login";
+import { onAuthChanged } from "./firebase";
+import { auth } from "./firebase";
+import { useEffect } from "react";
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
-  const navigate=useNavigate()
-  useEffect(()=>{
-    onAuthChanged(auth => {
-      if(!auth && location.pathname !=='/login'){
-        navigate('/login')
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthChanged((auth) => {
+      if (!auth && location.pathname !== "/login") {
+        navigate("/login");
       }
-    })
-
-  },[])
+    });
+  }, []);
   return (
     <div className="relative flex">
       <Sidebar />
@@ -28,14 +35,16 @@ const App = () => {
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
-              <Route path="/" element={<Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
-              <Route path="/artists/:id" element={<ArtistDetails />} />
-              <Route path="/songs/:songid" element={<SongDetails />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
-              <Route path="/login" element={<LoginMobile/>} />
+               
+                <Route path="/" element={<Discover />} />
+                <Route path="/top-artists" element={<TopArtists />} />
+                <Route path="/top-charts" element={<TopCharts />} />
+                <Route path="/around-you" element={<AroundYou />} />
+                <Route path="/artists/:id" element={<ArtistDetails />} />
+                <Route path="/songs/:songid" element={<SongDetails />} />
+                <Route path="/search/:searchTerm" element={<Search />} />
+                <Route path="/login" element={<LoginMobile />} />
+              
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
